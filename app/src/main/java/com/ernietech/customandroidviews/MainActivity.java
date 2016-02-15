c7058b681f308c1534d594ac85556198541da957
Ernie <ernie@ernietech.com>
2016-02-15 16:17:29 -0500

    create blank android project

---
+++ app/src/main/java/com/ernietech/customandroidviews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ernietech.customandroidviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
